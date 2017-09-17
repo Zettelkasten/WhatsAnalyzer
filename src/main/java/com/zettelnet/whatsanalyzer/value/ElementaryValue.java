bd14e6bc10a575c5a5ee72f8b0084661b1215c3e
Zettelkasten <Zettelkasten@localhost>
2017-09-17 19:13:17 +0200

    Add TOTAL_COUNT ElementaryValue

--- src/main/java/com/zettelnet/whatsanalyzer/value/ElementaryValue.java
+++ src/main/java/com/zettelnet/whatsanalyzer/value/ElementaryValue.java
@@ -8,14 +8,18 @@ import com.zettelnet.whatsanalyzer.ChatMessageType;
 
 public class ElementaryValue {
 
+	public static ToDoubleFunction<ChatMessage> TOTAL_COUNT = (ChatMessage message) -> {
+		return 1;
+	};
+
 	public static ToDoubleFunction<ChatMessage> MESSAGE_COUNT = (ChatMessage message) -> {
 		return (message.getType() == ChatMessageType.MESSAGE) ? 1 : 0;
 	};
-	
+
 	public static ToDoubleFunction<ChatMessage> MEDIUM_COUNT = (ChatMessage message) -> {
 		return (message.getType() == ChatMessageType.MEDIUM) ? 1 : 0;
 	};
-	
+
 	public static ToDoubleFunction<ChatMessage> LOCATION_COUNT = (ChatMessage message) -> {
 		return (message.getType() == ChatMessageType.LOCATION) ? 1 : 0;
 	};
@@ -28,7 +32,7 @@ public class ElementaryValue {
 		LocalTime time = message.getTime().toLocalTime();
 		return (time.getHour() <= 6 && time.getHour() >= 0) ? 1 : 0;
 	};
-	
+
 	public static ToDoubleFunction<ChatMessage> FML = (ChatMessage message) -> {
 		return message.getContent().toLowerCase().contains("fml") ? 1 : 0;
 	};
